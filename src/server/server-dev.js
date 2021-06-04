b9f8f0dedc21a0f7c1729f05d7c7cf6b933fbec3
metin <metin.ozkn@hotmail.com>
2021-06-04 10:54:25 +0300

    added hot module reloading

--- src/server/server-dev.js
+++ src/server/server-dev.js
@@ -2,6 +2,7 @@ import path from "path";
 import express from "express";
 import webpack from "webpack";
 import webpackDevMiddleware from "webpack-dev-middleware";
+import webpackHotMiddleware from "webpack-hot-middleware";
 import config from "../../webpack.dev.config.js";
 const app = express(),
   DIST_DIR = __dirname,
@@ -12,6 +13,7 @@ app.use(
     publicPath: config.output.publicPath,
   })
 );
+app.use(webpackHotMiddleware(compiler));
 app.get("*", (req, res, next) => {
   compiler.outputFileSystem.readFile(HTML_FILE, (err, result) => {
     if (err) {
--- webpack.dev.config.js
+++ webpack.dev.config.js
@@ -3,7 +3,10 @@ const webpack = require("webpack");
 const HtmlWebPackPlugin = require("html-webpack-plugin");
 module.exports = {
   entry: {
-    main: "./src/index.js",
+    main: [
+      "webpack-hot-middleware/client?path=/__webpack_hmr&timeout=20000",
+      "./src/index.js",
+    ],
   },
   output: {
     path: path.join(__dirname, "dist"),
@@ -47,6 +50,7 @@ module.exports = {
       filename: "./index.html",
       excludeChunks: ["server"],
     }),
+    new webpack.HotModuleReplacementPlugin(),
     new webpack.NoEmitOnErrorsPlugin(),
   ],
 };
